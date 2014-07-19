060abb7c216a07e4d5dcb7bc0725ffdee1117fe1
harute <kaizar19@gmail.com>
2014-07-19 16:29:33 +0900

    一旦コミット

--- src/com/example/pazudoraparty/PartySearch.java
+++ src/com/example/pazudoraparty/PartySearch.java
@@ -1,15 +1,37 @@
 package com.example.pazudoraparty;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteCursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 
 public class PartySearch extends Activity {
+
+    private DatabaseHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_party_search);
+
+        helper = new DatabaseHelper(this);
+        SQLiteDatabase db = helper.getReadableDatabase();
+        String sql = "select local_dish from local_dishes where prefecture = '北海道';";
+        Cursor c = db.rawQuery(sql, null);
+        //Cursor c = db.query("LOCAL_DISHES", new String[] {"prefecture"}, null, null, null, null, null);
+        c.moveToFirst();
+
+        CharSequence[] list = new CharSequence[c.getCount()];
+        for (int i = 0; i < list.length; i++) {
+            //list[i] = c.getString(0);
+            Log.v("Tag", c.getString(0));
+            c.moveToNext();
+        }
+        c.close();
+
+        //TextView txtView = (TextView)findViewById(R.id.txtView);
+        //txtView.setText(mon.monGet());
     }
 }
---
+++ src/com/example/pazudoraparty/searchMon.java
@@ -0,0 +1,16 @@
+package com.example.pazudoraparty;
+
+public class searchMon {
+    private String txt = "";
+
+    // コンストラクタ
+    searchMon(String t) {
+        txt = t;
+    }
+
+    // get
+    String monGet() {
+        return txt;
+    }
+}
+
