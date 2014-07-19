361c4709953db0e8d5d45c8be544a445f9e8315d
harute <kaizar19@gmail.com>
2014-07-19 17:02:48 +0900

    search導入。無駄なコードあり

--- src/com/example/pazudoraparty/PartySearch.java
+++ src/com/example/pazudoraparty/PartySearch.java
@@ -1,15 +1,77 @@
 package com.example.pazudoraparty;
 
 import android.app.Activity;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteCursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.TextUtils;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
 
-public class PartySearch extends Activity {
+public class PartySearch extends Activity implements OnQueryTextListener {
+    private String[] array_adapter_data = { "Apple", "Bike", "Cupcake",
+            "Donut", "Eclair", "Froyo", "Gingerbread", "Honeycomb" };
 
-    private DatabaseHelper helper;
+    private ListView list;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_party_search);
+
+        SearchView search = (SearchView) findViewById(R.id.searchView1);
+        list = (ListView) findViewById(R.id.listView1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, array_adapter_data);
+        list.setAdapter(adapter);
+        list.setTextFilterEnabled(true);
+
+        // SearchViewの初期表示状態を設定
+        search.setIconifiedByDefault(false);
+
+        // SearchViewにOnQueryChangeListenerを設定
+        //search.setOnQueryChangeListener(this);
+
+        // SearchViewのSubmitボタンを使用不可にする
+        search.setSubmitButtonEnabled(true);
+
+        // SearchViewに何も入力していない時のテキストを設定
+        search.setQueryHint("検索文字を入力して下さい。");
+    }
+
+    // SearchViewにテキストを入力する度に呼ばれるイベント
+    public boolean onQueryTextChanged(String queryText) {
+        if (TextUtils.isEmpty(queryText)) {
+            list.clearTextFilter();
+        } else {
+            list.setFilterText(queryText.toString());
+        }
+        return true;
+    }
+
+    // SearchViewのSubmitButtonを押下した時に呼ばれるイベント
+    public boolean onSubmitQuery(String queryText) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        // TODO 自動生成されたメソッド・スタブ
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        // TODO 自動生成されたメソッド・スタブ
+        if (TextUtils.isEmpty(newText)) {
+            list.clearTextFilter();
+        } else {
+            list.setFilterText(newText.toString());
+        }
+        return true;
+    }
+
+    /*private DatabaseHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +95,5 @@ public class PartySearch extends Activity {
 
         //TextView txtView = (TextView)findViewById(R.id.txtView);
         //txtView.setText(mon.monGet());
-    }
+    }*/
 }
